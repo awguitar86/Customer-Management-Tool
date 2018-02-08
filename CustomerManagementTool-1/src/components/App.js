6b10244a8cfc76ad8fac5e18d63d78e28b890f0e
Austin Wright <awguitar86@gmail.com>
2018-02-08 09:56:11 -0700

    adding all files and folders from this folder to the new repo

---
+++ CustomerManagementTool-1/src/components/App.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Header from './Header/Header';
+import List from './List/List';
+import Workspace from './Workspace/Workspace';
+
+import { getCustomerList, postCustomer, getCustomer, updateCustomer, deleteCustomer } from '../customers';
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      customerList: undefined,
+      initialLoad: true,
+      creating: false,
+      currentCustomer: null
+    }
+
+    this.startNewCustomer = this.startNewCustomer.bind(this);
+    this.createCustomer = this.createCustomer.bind(this);
+    this.selectCustomer = this.selectCustomer.bind(this);
+    this.saveEdit = this.saveEdit.bind(this);
+    this.removeCustomer = this.removeCustomer.bind(this);
+  }
+
+  componentDidMount() {
+    getCustomerList().then(list => {
+      this.setState({customerList: list});
+    })
+  }
+
+  startNewCustomer() {
+    this.setState({
+      creating: true,
+      initialLoad: false,
+      currentCustomer: null
+    })
+  }
+
+  createCustomer(customer) {
+    postCustomer(customer).then(response => {
+      getCustomerList().then(list => {
+        this.setState({
+          initialLoad:true,
+          creating: false,
+          customerList: list
+        })
+      })
+    })
+  }
+
+  selectCustomer(id) {
+    getCustomer(id).then(response =>
+      this.setState({
+        currentCustomer: response,
+        initalLoad: false
+    }))
+  }
+
+  saveEdit(id, obj) {
+    updateCustomer(id, obj).then(response => {
+      getCustomerList().then(list => {
+        this.setState({
+          customerList: list,
+          currentCustomer: updateCustomer
+        })
+      })
+    })
+  }
+
+  removeCustomer(id) {
+    deleteCustomer(id).then(list => {
+      getCustomerList().then(list => {
+        this.setState({
+          customerList: list,
+          currentCustomer: null,
+          initialLoad: true
+        })
+      })
+    })
+  }
+
+  render() {
+    var {
+      selectCustomer,
+      startNewCustomer
+    } = this.props
+
+    return (
+      <div>
+        <Header />
+        <div className="App__container">
+          {
+            this.state.customerList ?
+            <List
+              customerList={this.state.customerList || []}
+              startNewCustomer={this.startNewCustomer}
+              selectCustomer={this.selectCustomer}
+              />
+            : null
+          }
+          <Workspace initialLoad={this.state.initialLoad}
+                    createCustomer={this.createCustomer}
+                    currentCustomer={this.state.currentCustomer}
+                    creating={this.state.creating}
+                    saveEdit={this.saveEdit}
+                    removeCustomer={this.removeCustomer}
+                  />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
