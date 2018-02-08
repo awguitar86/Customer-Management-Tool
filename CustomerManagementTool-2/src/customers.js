b457f45ee1668740f83cd8bc49bf7e1eaeb60d9a
Austin Wright <awguitar86@gmail.com>
2018-02-08 13:38:14 -0700

    changes

--- CustomerManagementTool-2/src/customers.js
+++ CustomerManagementTool-2/src/customers.js
@@ -2,26 +2,31 @@ import axios from 'axios';
 import apiURL from './api';
 
 export const getCustomerList = () => {
-    axios.get(apiURL)
-        .then(res => res.data);
+    return axios.get(apiURL).then(res => {
+            return res.data;
+        })
 }
 
 export const postCustomer = (customer) => {
-    axios.post(apiURL, customer)
-        .then( res => res.data);
+    return axios.post(apiURL, customer).then( res => {
+        return res.data
+    });
 }
 
 export const getCustomer = (id) => {
-    axios.get(apiURL + id)
-        .then( res =>  res.data);
+    return axios.get(apiURL + id).then( res =>  {
+        return res.data
+    });
 }
 
 export const updateCustomer = (id, obj) => {
-    axios.patch(apiURL + id, obj)
-        .then( res => res.data);
+    return axios.patch(apiURL + id, obj).then( res => {
+        return res.data
+    });
 }
 
 export const deleteCustomer = (id) => {
-    axios.delete(apiURL + id)
-        .then( res => res.data);
+    return axios.delete(apiURL + id).then( res => {
+        return res.data
+    });
 }
