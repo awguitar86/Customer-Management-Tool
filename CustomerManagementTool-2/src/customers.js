2dd8c2c49d4338013160df7dc3c7f43587ab3b1b
Austin Wright <awguitar86@gmail.com>
2018-02-08 12:14:29 -0700

    created postCustomer function for creating new customer

--- CustomerManagementTool-2/src/customers.js
+++ CustomerManagementTool-2/src/customers.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 import apiURL from './api';
 
 export const getCustomerList = () => {
-    axios.get('apiURL')
+    axios.get(apiURL)
         .then(res => res.data);
+}
+
+export const postCustomer = (customer) => {
+    axios.post(apiURL, customer)
+        .then( res => res);
 }
